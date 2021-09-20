2edee5e7534d5db49547c2a0426656f16eec710e
Ben Coburn <ben@roonlabs.com>
2021-09-20 12:43:58 -0400

    add initial implementation of share listing

--- src/roon_watcher.c
+++ src/roon_watcher.c
@@ -7,6 +7,13 @@
 
 #include "bdsm.h"
 
+struct credentials
+{
+  char *workgroup;
+  char *username;
+  char *password;
+};
+
 static void print_entry(const char *what, void *p_opaque,
                         netbios_ns_entry *entry)
 {
@@ -22,10 +29,78 @@ static void print_entry(const char *what, void *p_opaque,
     netbios_ns_entry_type(entry));
 }
 
+static int list_shares(void *p_opaque,
+                       netbios_ns_entry *entry)
+{
+  struct credentials *creds = (struct credentials *)p_opaque;
+  struct in_addr  addr;
+  smb_session   *session;
+  smb_tid     tid;
+  smb_fd      fd;
+
+  session = smb_session_new();
+  if (session == NULL)
+    return 1;
+
+  addr.s_addr = netbios_ns_entry_ip(entry);
+
+  if (smb_session_connect(session, netbios_ns_entry_name(entry), 
+      addr.s_addr, SMB_TRANSPORT_TCP))
+  {
+    printf("Unable to connect to host %s\n", inet_ntoa(addr));
+    return 2;
+  }
+
+  smb_session_set_creds(session, creds->workgroup, creds->username, creds->password);
+  int login_ret = smb_session_login(session);
+  if (login_ret == DSM_SUCCESS)
+  {
+    if (smb_session_is_guest(session))
+      printf("Logged in as GUEST\n");
+    else
+      printf("Successfully logged in\n");
+  }
+  else
+  {
+    printf("Auth failed %s\n", inet_ntoa(addr));
+    return 3;
+  }
+
+  smb_share_list list;
+  size_t count;
+  int list_ret = smb_share_get_list(session, &list, &count);
+  if (list_ret == DSM_SUCCESS)
+  {
+    printf("    share count: %i\n", count);
+  }
+  else
+  {
+    printf("Unable to connect to share, ret value: %i\n", list_ret);
+    if (list_ret == DSM_ERROR_NT)
+    {
+      uint32_t nt_status = smb_session_get_nt_status(session);
+      printf("nt_status: %x\n", nt_status);
+    }
+      
+    return 4;
+  }
+
+  for (int i = 0; i < count; i++) {
+    printf("    share name: %s\n", smb_share_list_at(list, i));
+  }
+  
+  smb_share_list_destroy(list);
+  smb_session_destroy(session);
+
+  return 0;
+}
+
 static void on_entry_added(void *p_opaque,
                            netbios_ns_entry *entry)
 {
   print_entry("added", p_opaque, entry);
+
+  int list_ret = list_shares(p_opaque, entry);
 }
 
 static void on_entry_removed(void *p_opaque,
@@ -34,14 +109,40 @@ static void on_entry_removed(void *p_opaque,
   print_entry("removed", p_opaque, entry);
 }
 
-int main()
+int main(int argc, char** argv)
 {
+  struct credentials *args = malloc(sizeof(struct credentials));
+  if (argc >= 4)
+  {
+    args->workgroup = argv[1];
+    args->username  = argv[2];
+    args->password  = argv[3];
+  }
+  else if (argc == 3)
+  {
+    args->workgroup = "";
+    args->username  = argv[1];
+    args->password  = argv[2];
+  }
+  else if (argc == 2)
+  {
+    args->workgroup = "";
+    args->username  = argv[1];
+    args->password  = "";
+  }
+  else if (argc == 1)
+  {
+    args->workgroup = "";
+    args->username  = "";
+    args->password  = "";
+  }
+  
   netbios_ns *ns;
   netbios_ns_discover_callbacks callbacks;
 
   ns = netbios_ns_new();
 
-  callbacks.p_opaque = (void*)0x42;
+  callbacks.p_opaque = (void*)&args;
   callbacks.pf_on_entry_added = on_entry_added;
   callbacks.pf_on_entry_removed = on_entry_removed;
 
